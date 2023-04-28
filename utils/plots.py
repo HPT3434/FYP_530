271b3638a7d2a05309c587a4f0d6e3393b41b393
Ansonxddd <ansonlawss@gmail.com>
2023-04-28 21:33:16 +0800

    Update the plot.py

--- utils/plots.py
+++ utils/plots.py
@@ -267,16 +267,20 @@ def plot_labels(labels, save_dir=''):
     c, b = labels[:, 0], labels[:, 1:].transpose()  # classes, boxes
     nc = int(c.max() + 1)  # number of classes
 
-    fig, ax = plt.subplots(2, 2, figsize=(8, 8), tight_layout=True)
+    fig, ax = plt.subplots(1, 3, figsize=(8, 8), tight_layout=True)
     ax = ax.ravel()
     ax[0].hist(c, bins=np.linspace(0, nc, nc + 1) - 0.5, rwidth=0.8)
+    ax[0].set_title('Number of classes')
     ax[0].set_xlabel('classes')
+    fig.suptitle('Dataset labels', fontsize=16)
     ax[1].scatter(b[0], b[1], c=hist2d(b[0], b[1], 90), cmap='jet')
     ax[1].set_xlabel('x')
     ax[1].set_ylabel('y')
+    ax[1].set_title('x & y of boxes')
     ax[2].scatter(b[2], b[3], c=hist2d(b[2], b[3], 90), cmap='jet')
     ax[2].set_xlabel('width')
     ax[2].set_ylabel('height')
+    ax[2].set_title('width & height of boxes')
     plt.savefig(Path(save_dir) / 'labels.png', dpi=200)
     plt.close()
 
